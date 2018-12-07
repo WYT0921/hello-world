5366bfbff8725ab0db8a538575b3859ef1cc8397
WYT0921 <2637690455@qq.com>
2018-12-07 22:36:55 +0800

    add sushu.c

---
+++ sushu.c
@@ -0,0 +1,25 @@
+//判断素数 
+#include <stdio.h>
+int main()
+{
+	int i,a,b;
+	scanf("%d",&a);
+	for(i=2;i<a;i++)
+	{
+		b=a%i;
+		if(b==0)
+		{
+			break;
+		} 
+	} 
+		if(b==0)
+		{
+			printf("%d不是素数",a);
+		}
+		else
+		{
+			printf("%d是素数",a);
+		}
+	return 0;
+}
+ 
